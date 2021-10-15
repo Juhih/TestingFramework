275e3b4653a3448c9fd8f41469eb336df44867ec
Juhi Hazari <juhi.hazari@gmail.com>
2021-10-15 14:53:29 +0100

    Added capabilities of IE and Firefox browser

--- TestingFramework/Helper/DriverInitialize.cs
+++ TestingFramework/Helper/DriverInitialize.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Remote;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,19 +23,30 @@ namespace TestingFramework
             if (browser.ToLower().Equals("chrome"))
             {
                 new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.AddArgument("--start-maximized");
+                driver = new ChromeDriver(options);
             }
 
             else if (browser.ToLower().Equals("internetexplorer"))
             {
                 new WebDriverManager.DriverManager().SetUpDriver(new InternetExplorerConfig());
-                driver = new InternetExplorerDriver();
+                var options = new InternetExplorerOptions()
+                {
+                    IntroduceInstabilityByIgnoringProtectedModeSettings = true,
+                    IgnoreZoomLevel = true,
+                    EnableNativeEvents = false
+                };
+                driver = new InternetExplorerDriver(options);
+                
             }
 
             else if (browser.ToLower().Equals("firefox"))
             {
                 new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
-                driver = new FirefoxDriver();
+                FirefoxOptions options = new FirefoxOptions();
+                options.BrowserExecutableLocation = ("C:\\Users\\Juhi.Hazari\\AppData\\Local\\Mozilla Firefox\\firefox.exe"); //This is the location where you have installed Firefox on your machine
+                driver = new FirefoxDriver(options);
             }
 
         }
